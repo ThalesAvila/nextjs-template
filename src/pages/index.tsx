be002b390eec37307d6107fa700319e919218cf0
Thales Avila <dev.thales.avila@gmail.com>
2021-07-08 08:28:56 -0300

    chore: next-images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Head from 'next/head'
+import face from '../assets/face.jpg'
 
 const Home: React.FC = () => {
+  console.log('face')
   return (
     <>
       <Head>Home</Head>
       <main>
         <h1>Hello Spark</h1>
+        <img src={String(face)} />
       </main>
     </>
   )
