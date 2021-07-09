86fdd680ae03896fb8a45fe44993beec3cbabf5c
Thales Avila <dev.thales.avila@gmail.com>
2021-07-09 20:52:08 -0300

    fix: fixing next-image for Next11 version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const withImages = require('next-images')
 
 module.exports = withImages({
+  fileExtensions: ['jpg', 'jpeg', 'png', 'gif'],
   reactStrictMode: true,
-  esModule: true
+  esModule: true,
+  images: {
+    disableStaticImages: true
+  },
+  webpack(config, options) {
+    return config
+  }
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,12 @@ import Head from 'next/head'
 import face from '../assets/face.jpg'
 
 const Home: React.FC = () => {
-  console.log('face')
   return (
     <>
       <Head>Home</Head>
       <main>
         <h1>Hello Spark</h1>
-        <img src={String(face)} />
+        <img src={face} />
       </main>
     </>
   )
